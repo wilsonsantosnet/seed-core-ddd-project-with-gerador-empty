72ac0f887f492ff28676a0a7add8cf277b7c0e09
Wilson Santos <wilsonsantosnet@gmail.com>
2018-05-17 16:56:15 -0300

    update Seed layout Front to Angular 6.0

--- Gerador.Gen/ConfigExternalResources.cs
+++ Gerador.Gen/ConfigExternalResources.cs
@@ -10,14 +10,14 @@ namespace Seed.Gen
     public class ConfigExternalResources
     {
 
-        private ExternalResource ConfigExternarResourcesTemplatesFrontBs4Angular20(bool replaceLocalFilesApplication)
+        private ExternalResource ConfigExternarResourcesTemplatesFrontBs4Angular60(bool replaceLocalFilesApplication)
         {
 
             return new ExternalResource
             {
                 ReplaceLocalFilesApplication = replaceLocalFilesApplication,
                 ResouceRepositoryName = "template-gerador-front-bs4-angular2.0",
-                ResourceUrlRepository = "https://github.com/wilsonsantosnet/template-gerador-front-bs4-angular2.0.git",
+                ResourceUrlRepository = "https://github.com/wilsonsantosnet/Seed-layout-front-bs4-angular6.0.git",
                 ResourceLocalPathFolderExecuteCloning = @"C:\Projetos\Outros\Repositorios",
                 ResourceLocalPathDestinationFolrderApplication = @"C:\Projetos\seed-core-ddd-project-with-gerador-empty\Gerador.Gen\Templates\Front",
             };
@@ -129,7 +129,7 @@ namespace Seed.Gen
                ConfigExternarResourcesFrameworkCommon(replaceLocalFilesApplication),
                ConfigExternarResourcesTemplatesFrontBs4Angular20(replaceLocalFilesApplication),
                ConfigExternarResourcesFrameworkAngula20Crud(replaceLocalFilesApplication),
-               ConfigExternarResourcesSeedLayoutBs4Angular20(replaceLocalFilesApplication),
+               ConfigExternarResourcesSeedLayoutBs4Angular60(replaceLocalFilesApplication),
                ConfigExternarResourcesSeedLayoutBs4Angular20OnlyThisFiles(replaceLocalFilesApplication)
 
             };
